84a0b3daaf0830dfc36560f0b192e8ff041f5d99
adityasidharta <aditya.sdrt@gmail.com>
2021-11-02 23:38:57 -0400

    [Adi] Update midterm

--- hw2/create.sql
+++ hw2/create.sql
@@ -1,28 +1,69 @@
-CREATE SCHEMA `CSVCatalog` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE `midterm`;
 
-USE CSVCatalog;
+USE midterm;
 
-CREATE TABLE `csvtables` (
-  `table_name` varchar(45) NOT NULL,
-  `path` varchar(100) NOT NULL,
-  PRIMARY KEY (`table_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `Physician` (
+  `physician_ID` varchar(256) NOT NULL,
+  `last_name` varchar(256),
+  `first_name` varchar(256),
+  `physician_type` enum('NP','MD','DO'),
+  PRIMARY KEY (`physician_ID`)
+);
 
-CREATE TABLE `csvcolumns` (
-  `table_name` varchar(45) NOT NULL,
-  `column_name` varchar(256) NOT NULL,
-  `type` enum('text','number') NOT NULL,
-  `not_null` tinyint(4) NOT NULL,
-  PRIMARY KEY (`table_name`,`column_name`),
-  CONSTRAINT `col_to_tbl` FOREIGN KEY (`table_name`) REFERENCES `csvtables` (`table_name`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `Patient` (
+    `patient_ID` varchar(256) NOT NULL,
+    `last_name` varchar(256),
+    `first_name` varchar(256),
+    `street1` varchar(256),
+    `city` varchar(256),
+    `state` varchar(2),
+    `postal_code` int,
+    PRIMARY KEY (`patient_ID`)
+);
 
-CREATE TABLE `csvindexes` (
-  `table_name` varchar(45) NOT NULL,
-  `column_name` varchar(45) NOT NULL,
-  `type` enum('PRIMARY','UNIQUE','INDEX') NOT NULL,
-  `index_name` varchar(45) NOT NULL,
-  `index_order` varchar(45) NOT NULL,
-  PRIMARY KEY (`table_name`,`column_name`),
-  CONSTRAINT `ind_to_col` FOREIGN KEY (`table_name`, `column_name`) REFERENCES `csvcolumns` (`table_name`, `column_name`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `Insurance_Company` (
+    `company_id` varchar(256) NOT NULL,
+    `insured_person` varchar(256) NOT NULL,
+    `phone_no` varchar(256),
+    `street1` varchar(256),
+    `city` varchar(256),
+    `state` varchar(2),
+    `postal_code` int,
+    PRIMARY KEY (`company_id`),
+    FOREIGN KEY (`insured_person`) REFERENCES `Patient` (`patient_ID`)
+);
+
+CREATE TABLE `Payment` (
+    `receipt_no` varchar(256) NOT NULL,
+    `paid_amount` double,
+    `paid_date` date,
+    `paid_type` ENUM('CC', 'Check', 'Transfer'),
+    `bill_no` varchar(256) NOT NULL,
+    PRIMARY KEY (`receipt_no`),
+    FOREIGN KEY (`bill_no`) REFERENCES `Bill` (`bill_no`)
+);
+
+CREATE TABLE `Bill` (
+    `bill_no` varchar(256) NOT NULL,
+    `amount_insured` float,
+    `amount_not_insured` float,
+    `bill_total` float,
+    `bill_date` date,
+    `bill_status` varchar(256),
+    PRIMARY KEY (`bill_no`)
+);
+
+CREATE TABLE `Appointment` (
+  `appt_ID` varchar(256) NOT NULL,
+  `appt_date` date,
+  `appt_time` timestamp,
+  `appt_duration` int,
+  `appt_reason` varchar(256),
+  `physician_ID` varchar(256) NOT NULL,
+  `patient_ID` varchar(256) NOT NULL,
+  `bill_no` varchar(256) NOT NULL,
+  PRIMARY KEY (`appt_ID`),
+  FOREIGN KEY (`physician_ID`) REFERENCES `Physician` (`physician_ID`) ON DELETE RESTRICT,
+  FOREIGN KEY (`patient_ID`) REFERENCES `Patient` (`patient_ID`) ON DELETE RESTRICT,
+  FOREIGN KEY (`bill_no`) REFERENCES `Bill` (`bill_no`) ON DELETE RESTRICT
+);
